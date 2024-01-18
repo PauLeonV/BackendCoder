e86fdef646f7e1af9e446453976c2ebcb9c22c58
Paula Leon <124013231+PauLeonV@users.noreply.github.com>
2024-01-17 19:18:29 -0500

    Update ProductManager.js

--- src/ProductManager.js
+++ src/ProductManager.js
@@ -82,3 +82,77 @@ manager.addProduct({
     code: 'JM001',
     stock: 25
 });
+manager.addProduct({
+    title: 'Juego de Mesa - Exploradores del Cosmos',
+    description: 'Es un juego de mesa emocionante que te sumerge en la emocionante y desafiante vida de un astronauta espacial.',
+    price: 225000,
+    thumbnail: 'imagenCosmos.jpg',
+    code: 'JM002',
+    stock: 12
+});
+
+manager.addProduct({
+    title: 'Juego de Mesa - Aventura en CandyLand',
+    description: 'Es un emocionante juego de mesa diseñado para transportarte a un mundo dulce y mágico lleno de golosinas, caramelos y desafíos deliciosos.',
+    price: 68000,
+    thumbnail: 'imagenCandy.jpg',
+    code: 'JM003',
+    stock: 21
+});
+manager.addProduct({
+    title: 'Juego de Rol - Vampiros La Mascarada',
+    description: 'En esta nueva edición del juego de rol Vampiro, encontrarás todo lo necesario para pasar un rato increíble con tus colegas.',
+    price: 37500,
+    thumbnail: 'imagenVampiros.jpg',
+    code: 'JR001',
+    stock: 14
+});
+manager.addProduct({
+    title: 'Juego de Rol - Hellora',
+    description: 'Explora todos los entornos, entra en las mismísimas mazmorras y visita ciudades y pueblos.',
+    price: 120000,
+    thumbnail: 'imagenHellora.jpg',
+    code: 'JR002',
+    stock: 10
+});
+manager.addProduct({
+    title: 'Juego de Rol - Warhammer',
+    description: 'Si quieres sumergirte en un pasado medieval de fantasía, Wathammer Fantasy rol es la mejor opción.',
+    price: 98000,
+    thumbnail: 'imagenWarha.jpg',
+    code: 'JR003',
+    stock: 7
+});
+manager.addProduct({
+    title: 'Juego de Mesa - Uno',
+    description: 'Uno es un juego de cartas clásico y popular que ha sido disfrutado por personas de todas las edades.',
+    price: 18000,
+    thumbnail: 'imagenUno.jpg',
+    code: 'JM004',
+    stock: 50
+});
+manager.addProduct({
+    title: 'Juego de Mesa - Monopoly',
+    description: 'Juego de mesa icónico que ha entretenido a millones de personas desde su creación en la década de 1930. En este juego de estrategia.',
+    price: 69000,
+    thumbnail: 'imagenMonopoly.jpg',
+    code: 'JM005',
+    stock: 47
+});
+manager.addProduct({
+    title: 'Juego de Mesa - Parchis',
+    description: 'El parchís es un juego de mesa español derivado y similar al Eile mit Weile, al parqués. Es muy popular en España, Europa y Marruecos.',
+    price: 27500,
+    thumbnail: 'imagenParchis.jpg',
+    code: 'JM006',
+    stock: 75
+});
+manager.addProduct({
+    title: 'Juego de Rol - Fanhunter',
+    description: 'Se trata de un juego de rol epidecadente ambientado en un futuro próximo, donde un villano cree estar poseído por el espíritu de un escritor.',
+    price: 45800,
+    thumbnail: 'imagenFanhunter.jpg',
+    code: 'JR004',
+    stock: 13
+});
+
