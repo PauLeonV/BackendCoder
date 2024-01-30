4e129f0acd8eedf6dcbd89ea2897c8adf8fbac10
Paula Leon <124013231+PauLeonV@users.noreply.github.com>
2024-01-29 23:13:30 -0500

    Update app.js

--- entrega1/src/app.js
+++ entrega1/src/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { productsRouter } from './routes/products.router.js';
 import { cartsRouter } from './routes/carts.router.js';
+import bodyParser from 'body-parser';
 const app = express();
 const port = 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(bodyParser.json());
 
 app.use(express.static('./public'));
 app.use('/api/products', productsRouter);
