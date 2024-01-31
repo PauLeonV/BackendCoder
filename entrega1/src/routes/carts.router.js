f26605c3db709b1b5b2f658259cde9a5ba227b9e
Paula Leon <124013231+PauLeonV@users.noreply.github.com>
2024-01-30 22:19:05 -0500

    Update carts.router.js

--- entrega1/src/routes/carts.router.js
+++ entrega1/src/routes/carts.router.js
@@ -3,23 +3,26 @@ import { CartManager } from './CartManager.js';
 const cartsRouter = express.Router();
 const cartManager = new CartManager();
 
+
 cartsRouter.post('/', (req, res)=>{
-  const newCart = cartManager.createCart(req.body);
+  const products = req.body;
+  const newCart = cartManager.createCart(products);
+  console.log (newCart.id);
   res.json(newCart);
 });
 
 cartsRouter.get('/:cid', (req,res)=>{
-  const cartId = req.params.cid;
+  const cartId = parseInt(req.params.cid);
   const cart = cartManager.getCartById(cartId);
   if (cart){
     res.json(cart);
   } else {
-    res.status(404).send('Product not found');
+    res.status(404).send('Cart not found');
   }
 });
 cartsRouter.post('/:cid/product/:pid', (req, res) => {
-  const cartId = req.params.cid;
-  const productId = req.params.pid;
+  const cartId = parseInt(req.params.cid);
+  const productId = parseInt(req.params.pid);
   const quantity = req.body.quantity || 1;
   cartManager.addProductToCart(cartId, productId, quantity, res);
 });
